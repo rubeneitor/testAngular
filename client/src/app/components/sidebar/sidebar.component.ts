28fcdd1c851ff5ae57a86c3a6f59450227365900
rubeneitor <rvb1492@hotmail.com>
2025-02-22 13:41:33 +0100

    feat: sidebar
    - Componente para el sidebar
    - Accesible desde app.component
    - Funcionalidad versátil para versión móvil y versión PC

---
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter, HostListener, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-sidebar',
+    imports: [CommonModule, RouterModule],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent implements OnInit {
+    sidebarVisible = true;
+    isMobile = false;
+
+    ngOnInit() {
+        if (typeof window !== 'undefined') { 
+            this.checkScreenSize();
+            window.addEventListener('resize', () => this.checkScreenSize());
+        }
+    }
+
+    // Evento para notificar al padre (app.component)
+    @Output() sidebarToggled = new EventEmitter<boolean>();
+
+
+    checkScreenSize() {
+        this.isMobile = window.innerWidth < 768; 
+    }
+
+    hideSidebar() {
+        if (this.isMobile) {
+            this.sidebarVisible = false;
+        }
+        this.sidebarToggled.emit(this.sidebarVisible);
+    }
+
+    hiderSidebarButton() {
+        this.sidebarVisible = false;
+        this.sidebarToggled.emit(this.sidebarVisible);
+    }
+
+    toggleSidebar() {
+        this.sidebarVisible = !this.sidebarVisible;
+        this.sidebarToggled.emit(this.sidebarVisible); // Emitir cambio
+    }
+
+}
